3224ce9832dd690d9fbafa645e2ccbfabef02795
Andre Gardi <gardikoury@gmail.com>
2018-09-28 16:50:42 -0300

    blue chart U$ format

--- myChart.js
+++ myChart.js
@@ -1,3 +1,7 @@
+function float2dollar(value){
+    return "U$ "+(value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
 function renderChart(data, labels) {
     var ctx = document.getElementById("myChart").getContext('2d');
     var myChart = new Chart(ctx, {
@@ -7,7 +11,21 @@ function renderChart(data, labels) {
             datasets: [{
                 label: 'This week',
                 data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
             }]
+        },
+        options: {            
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function(value, index, values) {
+                            return float2dollar(value);
+                        }
+                    }
+                }]                
+            }
         },
     });
 }
@@ -15,7 +33,7 @@ function renderChart(data, labels) {
 $("#renderBtn").click(
     function () {
         data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
-        labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+        labels = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
         renderChart(data, labels);
     }
 );
